6f26f2b71b42685f6e73acd6918c5b806417277d
Thiago Diniz <thiagodinizf@gmail.com>
2018-04-11 09:40:36 -0300

    Initial Commit

---
+++ POC-Tabmenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POC-Tabmenu
+//
+//  Created by Thiago Diniz on 11/04/18.
+//  Copyright © 2018 Thiago Diniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
